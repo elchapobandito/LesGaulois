59c974c478491038cac4579a6affcb45745b2444
elchapobandito <lucas.bailliot@univ-tlse3.fr>
2024-09-23 23:59:03 +0200

    vk

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -14,29 +14,28 @@ public class Gaulois {
 	public String getNom() {
 		return nom;
 	}
-	
+
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "<< " + texte + ">>");
 	}
-	
+
 	private String prendreParole() {
 		return "Le gaulois " + nom + " : ";
 	}
-	
+
 	public void frapper(Romain romain) {
-		System.out.println(nom + "envoie un grand coup dans la mâchoire de "
-			+ romain.getNom());
+		System.out.println(nom + "envoie un grand coup dans la mâchoire de " + romain.getNom());
 		romain.recevoirCoup(force / 3);
 	}
 
-	/*@Override
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetpotion=" + effetpotion + "]";
-	}*/
-	
+	/*
+	 * @Override public String toString() { return "Gaulois [nom=" + nom +
+	 * ", force=" + force + ", effetpotion=" + effetpotion + "]"; }
+	 */
+
 	public static void main(String[] args) {
 		new Gaulois("Astérix", 8);
 		System.out.println("Astérix");
-		//TODO creer un main permettant de tester la classe Gaulois
+		// TODO creer un main permettant de tester la classe Gaulois
 	}
 }
